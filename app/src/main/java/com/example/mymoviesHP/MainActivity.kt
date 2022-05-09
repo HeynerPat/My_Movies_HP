3251fd68a3751868261887d35167ba10a80997e3
HeynerPat <72895524+HeynerPat@users.noreply.github.com>
2022-05-09 16:37:44 -0500

    Box - Layouts

--- app/src/main/java/com/example/mymoviesHP/MainActivity.kt
+++ app/src/main/java/com/example/mymoviesHP/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.mymoviesHP
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mymoviesHP.ui.theme.MyMoviesHPTheme
@@ -29,16 +32,28 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true, name="Android Greeting")
+@Preview(
+    showBackground = true,
+    name = "Android Greeting",
+    widthDp = 400,
+    heightDp = 200
+)
 @Composable
 fun DefaultPreview() {
     MyMoviesHPTheme {
-        Greeting("Heyner")
-        Greeting("Android")
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Greeting("Heyner")
+            Greeting("Android", modifier = Modifier.align(Alignment.BottomEnd))
+        }
+
     }
 }
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
+}
+
