1efed6cb7d7b97ef51186cd3eec98f2938533e88
HeynerPat <72895524+HeynerPat@users.noreply.github.com>
2022-05-15 14:06:32 -0500

    Agregar Imagen e Icono

--- app/src/main/java/com/example/mymoviesHP/MainActivity.kt
+++ app/src/main/java/com/example/mymoviesHP/MainActivity.kt
@@ -3,19 +3,27 @@ package com.example.mymoviesHP
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.PlayCircleOutline
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -27,6 +35,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
+import coil.transform.BlurTransformation
+import coil.transform.CircleCropTransformation
 import com.example.mymoviesHP.ui.theme.MyMoviesHPTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,7 +50,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ButtonText()
+                    MediaItem()
                 }
             }
         }
@@ -52,10 +63,22 @@ fun MediaItem(){
         Box(
             modifier = Modifier
                 .height(200.dp)
-                .fillMaxWidth()
-                .background(color = Color.Red)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
         ) {
-
+            Image(
+                painter = rememberImagePainter(
+                    data = "https://images4.alphacoders.com/909/909005.jpg",
+                ),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+            Icon(imageVector = Icons.Default.PlayCircleOutline,
+                contentDescription = null,
+                modifier = Modifier.size(92.dp),
+                tint = Color.White,
+            )
         }
         Box(
             contentAlignment = Alignment.Center,
